e2c214467e74a253f1d8928ecc2b2b002cc5d4db
Dharma Putra <dharmatkjone@gmai.com>
2022-09-13 22:53:16 +0700

    test

--- plugins/check-header/check-header-0.0-2.rockspec
+++ plugins/check-header/check-header-0.0-2.rockspec
@@ -11,15 +11,18 @@ description = {
   summary = "A Kong plugin, that extract roles from a JWT token and make a request for a Policy Decision Point (PDP)",
   license = "GPL"
 }
-supported_platforms = {
-   "linux",
-   "macosx"
+dependencies = {
+  "lua ~> 5.1",
+  "json4lua ~> 0.9.30-1"
 }
-dependencies = {}
 build = {
-   type = "builtin",
-   modules = {
-      ["kong.plugins.check-header.schema"] = "kong/plugins/"..pluginName.."/handler.lua",
-      ["kong.plugins.check-header.handler"] = "kong/plugins/"..pluginName.."/schema.lua" 
-   }
+  type = "builtin",
+  modules = {
+    ["kong.plugins."..pluginName..".pepmain"] = "kong/plugins/"..pluginName.."/pepmain.lua",
+    ["kong.plugins."..pluginName..".handler"] = "kong/plugins/"..pluginName.."/handler.lua",
+    ["kong.plugins."..pluginName..".schema"] = "kong/plugins/"..pluginName.."/schema.lua",
+    ["kong.plugins."..pluginName..".utils"] = "kong/plugins/"..pluginName.."/utils.lua",
+    ["kong.plugins."..pluginName..".pdpsender"] = "kong/plugins/"..pluginName.."/pdpsender.lua",
+    ["kong.plugins."..pluginName..".pdputils"] = "kong/plugins/"..pluginName.."/pdputils.lua"
+  }
 }
--- plugins/check-header/kong/plugins/check-header/handler.lua
+++ plugins/check-header/kong/plugins/check-header/handler.lua
@@ -1,68 +1,15 @@
 local BasePlugin = require "kong.plugins.base_plugin"
+local access = require "kong.plugins.pepkong.pepmain"
 
-local type = type
-local error = error
-local insert = table.insert
-local tostring = tostring
-local setmetatable = setmetatable
-local getmetatable = getmetatable
+local PEPPlugin = BasePlugin:extend()
 
-local HeadersValidationHandler = BasePlugin:extend()
-HeadersValidationHandler.PRIORITY = 100
-
-local function array_missing_value(tab, val)
-    for index, value in ipairs(tab) do
-        if value == val then
-            return false
-        end
-    end
-    return true
+function PEPPlugin:new()
+	PEPPlugin.super.new(self, "pepkong")
 end
 
-local function create_error(errors, header_name, error_message, error_code)
-    return {
-        header_name = header_name,
-        error_message = error_message,
-        error_code = error_code
-    }
+function PEPPlugin:access(conf)
+	PEPPlugin.super.access(self)
+	access.run(conf)	
 end
 
-local function validate_headers(conf)
-    local error = nil
-    for header_name, header_config in pairs(conf.headers) do
-        if header_config.http_methods[kong.request.get_method()] then
-            local header_value = kong.request.get_header(header_name)
-            if header_value == nil and header_config.required then
-                error = create_error(header_name, header_config.missing_header_response_text,
-                    header_config.missing_header_response_code)
-            else
-                if array_missing_value(header_config.values, header_value) then
-                    error = create_error(header_name, header_config.invalid_header_response_text,
-                        header_config.invalid_header_response_code)
-                end
-            end
-        end
-    end
-    return error
-end
-
-function HeadersValidationHandler:new()
-    HeadersValidationHandler.super.new(self, "headers-validation")
-end
-
-function HeadersValidationHandler:access(conf)
-    HeadersValidationHandler.super.access(self)
-
-    if kong.request.get_method() == "OPTIONS" then
-        return
-    end
-
-    local error = validate_headers(conf)
-    if not error == nil then
-        return kong.response.exit(error.code, {
-            message = error.message
-        })
-    end
-end
-
-return HeadersValidationHandler
+return PEPPlugin
