eccb643870ab28e5e789c8b9be77178ad0768b43
Dharma Putra <dharmatkjone@gmai.com>
2022-09-14 00:07:03 +0700

    test

--- plugins/check-header/kong/plugins/check-header/handler.lua
+++ plugins/check-header/kong/plugins/check-header/handler.lua
@@ -1,114 +1,82 @@
 
-local type = type
-local error = error
-local insert = table.insert
-local tostring = tostring
-local setmetatable = setmetatable
-local getmetatable = getmetatable
-
 local plugin = {
     PRIORITY = 1000, -- set the plugin priority, which determines plugin execution order
     VERSION = "0.1", -- version in X.Y.Z format. Check hybrid-mode compatibility requirements.
-}
+  }
   
-local err_list_mt = {}
-
-function table_to_string(tbl)
-    local result = ""
-    for k, v in pairs(tbl) do
-        -- Check the key type (ignore any numerical keys - assume its an array)
-        if type(k) == "string" then
-            result = result.."[\""..k.."\"]".."="
-        end
-
-        -- Check the value type
-        if type(v) == "table" then
-            result = result..table_to_string(v)
-        elseif type(v) == "boolean" then
-            result = result..tostring(v)
-        else
-            result = result.."\""..v.."\""
-        end
-        result = result..","
-    end
-    -- Remove leading commas from the result
-    if result ~= "" then
-        result = result:sub(1, result:len()-1)
-    end
-    return result
-end
-
-local function array_missing_value(tab, val)
-  for index, value in ipairs(tab) do
-      if value == val then
-          return false
-      end
-  end
-  return true
-end
-
-local function add_error(errors, k, v)
-  if not errors then
-    errors = {}
-  end
-
-  if errors and errors[k] then
-    if getmetatable(errors[k]) ~= err_list_mt then
-      errors[k] = setmetatable({errors[k]}, err_list_mt)
-    end
-
-    insert(errors[k], v)
-  else
-    errors[k] = v
-  end
-
-  return errors
-end
-
-local function validate_headers(conf)
-	local errors
-
-	for header_name,v in pairs(conf.headers) do
-		local header_value = kong.request.get_header(header_name)
-		if header_value == nil then
-			errors = add_error(errors, header_name, "is not present") 
-		-- elseif type(id) ~= "string" then
-			-- errors = add_error(errors, header_name, "must be a string")
-			-- kong.service.request.set_header("x-user-id", id)
-		-- return false, { status = 428, message = string.format("Required header '%s' is missing", header_name) }     
-		else
-			for htype,hvalues in pairs(v) do
-				if htype ~= type(header_value) then
-					errors = add_error(errors, header_name, string.format("has type %s but expected type was %s", type(header_value), htype)) 
-					-- return false, { status = 412, message = string.format("Header '%s' has type: %s. Expected type: %s", header_name, header_type, htype) }
-				elseif array_missing_value(hvalues, header_value) then
-					-- return false, { status = 412, message = string.format("Invalid value (%s) for header %s", header_value, header_name) }
-					errors = add_error(errors, header_name, string.format("has invalid value: %s", header_value)) 
-				end
-				break
-			end
-		end
-	end
-    -- return true
-    return errors == nil, errors
-end
-
-function HeadersValidationHandler:new()
-    HeadersValidationHandler.super.new(self, "headers-validation")
-end
-
-function HeadersValidationHandler:access(conf)
-    HeadersValidationHandler.super.access(self)
-
-    if kong.request.get_method() == "OPTIONS" then
-        return
-    end
-
-    local ok, errors = validate_headers(conf)
-    if not ok then
-		return kong.response.exit(412, { message = "Preconditions failed: " .. table_to_string(errors) })
-    end
-end
+  
+  
+-- do initialization here, any module level code runs in the 'init_by_lua_block',
+-- before worker processes are forked. So anything you add here will run once,
+-- but be available in all workers.
 
 
-return HeadersValidationHandler
+
+-- handles more initialization, but AFTER the worker process has been forked/created.
+-- It runs in the 'init_worker_by_lua_block'
+function plugin:init_worker()
+
+  -- your custom code here
+  kong.log.debug("saying hi from the 'init_worker' handler")
+
+end --]]
+
+
+
+--[[ runs in the 'ssl_certificate_by_lua_block'
+-- IMPORTANT: during the `certificate` phase neither `route`, `service`, nor `consumer`
+-- will have been identified, hence this handler will only be executed if the plugin is
+-- configured as a global plugin!
+function plugin:certificate(plugin_conf)
+  -- your custom code here
+  kong.log.debug("saying hi from the 'certificate' handler")
+end --]]
+
+
+
+--[[ runs in the 'rewrite_by_lua_block'
+-- IMPORTANT: during the `rewrite` phase neither `route`, `service`, nor `consumer`
+-- will have been identified, hence this handler will only be executed if the plugin is
+-- configured as a global plugin!
+function plugin:rewrite(plugin_conf)
+  -- your custom code here
+  kong.log.debug("saying hi from the 'rewrite' handler")
+end --]]
+
+
+
+-- runs in the 'access_by_lua_block'
+function plugin:access(plugin_conf)
+
+  -- your custom code here
+  kong.log.inspect(plugin_conf)   -- check the logs for a pretty-printed config!
+  kong.service.request.set_header(plugin_conf.request_header, "this is on a request")
+
+end --]]
+
+
+-- runs in the 'header_filter_by_lua_block'
+function plugin:header_filter(plugin_conf)
+
+  -- your custom code here, for example;
+  kong.response.set_header(plugin_conf.response_header, "this is on the response")
+
+end --]]
+
+
+--[[ runs in the 'body_filter_by_lua_block'
+function plugin:body_filter(plugin_conf)
+  -- your custom code here
+  kong.log.debug("saying hi from the 'body_filter' handler")
+end --]]
+
+
+--[[ runs in the 'log_by_lua_block'
+function plugin:log(plugin_conf)
+  -- your custom code here
+  kong.log.debug("saying hi from the 'log' handler")
+end --]]
+
+
+-- return our plugin object
+return plugin
--- plugins/check-header/kong/plugins/check-header/schema.lua
+++ plugins/check-header/kong/plugins/check-header/schema.lua
@@ -1,32 +1,42 @@
 local typedefs = require "kong.db.schema.typedefs"
 
 
-return {
-  name = "headers-validation",
+local PLUGIN_NAME = "check-header"
+
+
+local schema = {
+  name = PLUGIN_NAME,
   fields = {
+    -- the 'fields' array is the top-level entry with fields defined by Kong
+    { consumer = typedefs.no_consumer },  -- this plugin cannot be configured on a consumer (typical for auth plugins)
+    { protocols = typedefs.protocols_http },
     { config = {
+        -- The 'config' record is the custom part of the plugin schema
         type = "record",
         fields = {
-          { consumer = typedefs.no_consumer },  -- this plugin cannot be configured on a consumer (typical for auth plugins)
-          {
-            headers = {
-                type = "map",
-                keys = { type = "string", match_none = { {pattern = "^$", err = "Header name cannot be empty",}, }, },
-                values = {
-                    type = "map",
-                    match_none = { {pattern = "^$",err = "Header expected type and possible values (map) cannot be empty",}, },
-                    keys = { type = "string", match_none = { {pattern = "^$",err = "Header type (string or number) cannot be empty",}, }, },
-                    values = { 
-                      type = "array", 
-                      match_none = { {pattern = "^$",err = "Header expected values cannot be empty",}, },
-                      elements = { type = "string", required = true },
-                    },
-                },
-                default = {}
-            },
-         },
+          -- a standard defined field (typedef), with some customizations
+          { request_header = typedefs.header_name {
+              required = true,
+              default = "Hello-World" } },
+          { response_header = typedefs.header_name {
+              required = true,
+              default = "Bye-World" } },
+          { ttl = { -- self defined field
+              type = "integer",
+              default = 600,
+              required = true,
+              gt = 0, }}, -- adding a constraint for the value
+        },
+        entity_checks = {
+          -- add some validation rules across fields
+          -- the following is silly because it is always true, since they are both required
+          { at_least_one_of = { "request_header", "response_header" }, },
+          -- We specify that both header-names cannot be the same
+          { distinct = { "request_header", "response_header"} },
         },
       },
     },
   },
 }
+
+return schema
