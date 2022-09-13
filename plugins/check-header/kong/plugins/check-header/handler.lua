5f62456ed63de1e602639612403aa802bf92b8e7
Dharma Putra <dharmatkjone@gmai.com>
2022-09-14 00:04:41 +0700

    test

--- plugins/check-header/kong/plugins/check-header/handler.lua
+++ plugins/check-header/kong/plugins/check-header/handler.lua
@@ -11,6 +11,33 @@ local plugin = {
     VERSION = "0.1", -- version in X.Y.Z format. Check hybrid-mode compatibility requirements.
 }
   
+local err_list_mt = {}
+
+function table_to_string(tbl)
+    local result = ""
+    for k, v in pairs(tbl) do
+        -- Check the key type (ignore any numerical keys - assume its an array)
+        if type(k) == "string" then
+            result = result.."[\""..k.."\"]".."="
+        end
+
+        -- Check the value type
+        if type(v) == "table" then
+            result = result..table_to_string(v)
+        elseif type(v) == "boolean" then
+            result = result..tostring(v)
+        else
+            result = result.."\""..v.."\""
+        end
+        result = result..","
+    end
+    -- Remove leading commas from the result
+    if result ~= "" then
+        result = result:sub(1, result:len()-1)
+    end
+    return result
+end
+
 local function array_missing_value(tab, val)
   for index, value in ipairs(tab) do
       if value == val then
@@ -20,50 +47,68 @@ local function array_missing_value(tab, val)
   return true
 end
 
-local function create_error(errors, header_name, error_message, error_code)
-  return {
-      header_name = header_name,
-      error_message = error_message,
-      error_code = error_code
-  }
+local function add_error(errors, k, v)
+  if not errors then
+    errors = {}
+  end
+
+  if errors and errors[k] then
+    if getmetatable(errors[k]) ~= err_list_mt then
+      errors[k] = setmetatable({errors[k]}, err_list_mt)
+    end
+
+    insert(errors[k], v)
+  else
+    errors[k] = v
+  end
+
+  return errors
 end
 
 local function validate_headers(conf)
-  local error = nil
-  for header_name, header_config in pairs(conf.headers) do
-      if header_config.http_methods[kong.request.get_method()] then
-          local header_value = kong.request.get_header(header_name)
-          if header_value == nil and header_config.required then
-              error = create_error(header_name, header_config.missing_header_response_text,
-                  header_config.missing_header_response_code)
-          else
-              if array_missing_value(header_config.values, header_value) then
-                  error = create_error(header_name, header_config.invalid_header_response_text,
-                      header_config.invalid_header_response_code)
-              end
-          end
-      end
-  end
-  return error
+	local errors
+
+	for header_name,v in pairs(conf.headers) do
+		local header_value = kong.request.get_header(header_name)
+		if header_value == nil then
+			errors = add_error(errors, header_name, "is not present") 
+		-- elseif type(id) ~= "string" then
+			-- errors = add_error(errors, header_name, "must be a string")
+			-- kong.service.request.set_header("x-user-id", id)
+		-- return false, { status = 428, message = string.format("Required header '%s' is missing", header_name) }     
+		else
+			for htype,hvalues in pairs(v) do
+				if htype ~= type(header_value) then
+					errors = add_error(errors, header_name, string.format("has type %s but expected type was %s", type(header_value), htype)) 
+					-- return false, { status = 412, message = string.format("Header '%s' has type: %s. Expected type: %s", header_name, header_type, htype) }
+				elseif array_missing_value(hvalues, header_value) then
+					-- return false, { status = 412, message = string.format("Invalid value (%s) for header %s", header_value, header_name) }
+					errors = add_error(errors, header_name, string.format("has invalid value: %s", header_value)) 
+				end
+				break
+			end
+		end
+	end
+    -- return true
+    return errors == nil, errors
 end
 
 function HeadersValidationHandler:new()
-  HeadersValidationHandler.super.new(self, "headers-validation")
+    HeadersValidationHandler.super.new(self, "headers-validation")
 end
 
 function HeadersValidationHandler:access(conf)
-  HeadersValidationHandler.super.access(self)
+    HeadersValidationHandler.super.access(self)
 
-  if kong.request.get_method() == "OPTIONS" then
-      return
-  end
+    if kong.request.get_method() == "OPTIONS" then
+        return
+    end
 
-  local error = validate_headers(conf)
-  if not error == nil then
-      return kong.response.exit(error.code, {
-          message = error.message
-      })
-  end
+    local ok, errors = validate_headers(conf)
+    if not ok then
+		return kong.response.exit(412, { message = "Preconditions failed: " .. table_to_string(errors) })
+    end
 end
+
 
 return HeadersValidationHandler
--- plugins/check-header/kong/plugins/check-header/schema.lua
+++ plugins/check-header/kong/plugins/check-header/schema.lua
@@ -1,89 +1,32 @@
 local typedefs = require "kong.db.schema.typedefs"
 
 
-http_method = Schema.define {
-  type = "string",
-  match = "^(GET|HEAD|POST|PUT|DELETE|CONNECT|OPTIONS|TRACE|PATCH)$"
-}
-
-http_methods = Schema.define {
-  type = "set",
-  elements = http_method
-}
-
 return {
   name = "headers-validation",
-  fields = {{
-      config = {
-          type = "record",
-          fields = {{
-              consumer = typedefs.no_consumer
-          }, -- this plugin cannot be configured on a consumer (typical for auth plugins)
+  fields = {
+    { config = {
+        type = "record",
+        fields = {
+          { consumer = typedefs.no_consumer },  -- this plugin cannot be configured on a consumer (typical for auth plugins)
           {
-              rules = {
-                  type = "map",
-                  keys = {
-                      type = "string",
-                      match_none = {{
-                          pattern = "^$",
-                          err = "Rule name cannot be empty"
-                      }}
-                  },
-                  values = {
-                      type = "record",
-                      fields = {{
-                          http_methods = {
-                              type = "set",
-                              required = true,
-                              default = "*",
-                              elements = {
-                                  type = "string",
-                                  one_of = http_methods
-                              }
-                          }
-                      }, {
-                          invalid_header_response_code = {
-                              type = "integer",
-                              between = {100, 600},
-                              default = 400
-                          }
-                      }, {
-                          invalid_header_response_text = {
-                              type = "string",
-                              default = ""
-                          }
-                      }, {
-                          missing_header_response_code = {
-                              type = "integer",
-                              between = {100, 600},
-                              default = 400
-                          }
-                      }, {
-                          missing_header_response_text = {
-                              type = "string",
-                              default = ""
-                          }
-                      }, {
-                          required = {
-                              type = "boolean",
-                              default = false
-                          }
-                      }, {
-                          values = {
-                              type = "array",
-                              match_none = {{
-                                  pattern = "^$",
-                                  err = "Header expected values cannot be empty"
-                              }},
-                              elements = {
-                                  type = "string",
-                                  required = true
-                              }
-                          }
-                      }}
-                  }
-              }
-          }}
-      }
-  }}
+            headers = {
+                type = "map",
+                keys = { type = "string", match_none = { {pattern = "^$", err = "Header name cannot be empty",}, }, },
+                values = {
+                    type = "map",
+                    match_none = { {pattern = "^$",err = "Header expected type and possible values (map) cannot be empty",}, },
+                    keys = { type = "string", match_none = { {pattern = "^$",err = "Header type (string or number) cannot be empty",}, }, },
+                    values = { 
+                      type = "array", 
+                      match_none = { {pattern = "^$",err = "Header expected values cannot be empty",}, },
+                      elements = { type = "string", required = true },
+                    },
+                },
+                default = {}
+            },
+         },
+        },
+      },
+    },
+  },
 }
